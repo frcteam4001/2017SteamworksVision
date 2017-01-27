41506672f9ece7887d0ee648696371376cfcde41
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-01-27 20:03:59 +0000

    added zoning algorithm

--- Pipeline.py
+++ Pipeline.py
@@ -1,18 +1,63 @@
 import copy
+import logging
+from networktables import NetworkTable
 
-def process(frame):
+
+NetworkTable.setIPAddress("127.0.0.1")
+NetworkTable.setClientMode()
+NetworkTable.initialize()
+logging.basicConfig(level=logging.DEBUG)
+
+#wait for connection
+while not NetworkTable.isConnected:
+    pass
+
+table = NetworkTable.getTable("GearTable")
+
+
+# X values of the left boundary of zones, assuming the robot is at the correct distance
+impossibleLeft = 0
+impossibleRight = 0
+zone1 = 0
+zone2 = 0
+zone3 = 0
+zone4 = 0
+
+
+
+def Pipeline(frame):
+
+    #resize for faster processing/should be moved outside
     frame.resize(320, 240)
+
+    #makes a copy of the original image
     orig = copy.deepcopy(frame)
 
+    #applies filters
     frame.color_hls()
-    mask = frame.threshold([83, 69, 165], [100, 203, 207])
-    mask.blur(2)
+    mask = frame.threshold([70, 100, 50], [100, 190, 155])
+    mask.blur(3)
 
-    mask.contours_filter(area=(400, -1))
-    for idx, cnt in enumerate(mask.contours):
-        print('area: ' + cnt.area)
-        print('width', cnt.width)
-        print('height', cnt.height)
-        print('center_x', cnt.center_x)
-        print('center_y', cnt.center_y)
+    #if only 1 contour has been found, the robot must be very off
+    if len(mask.contours) == 1:
+        table.putValue('zone', -1)
+    elif len(mask.contours) == 2:
+        liftX = mask.contours[0].center_x + mask.contours[1].center_x
+
+
+        if liftX < impossibleLeft * 1.05:
+            table.putValue('zone', -1)
+        elif liftX * 1.05 > impossibleRight:
+            table.putValue('zone', -1)
+        elif zone1 <= liftX < zone2:
+            table.putValue('zone', 1)
+        elif zone2 <= liftX < zone3:
+            table.putValue('zone', 2)
+        elif zone3 <= liftX < zone4:
+            table.putValue('zone', 3)
+        else:
+            table.putValue('zone', 4)
+
+    mask.contours_draw(orig)
+
     return orig
