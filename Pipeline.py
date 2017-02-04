4cbb2ac2f18634077e354d777f0bb02a4c680583
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-02-04 16:32:00 -0500

    modified pipeline to take two largest contours

--- Pipeline.py
+++ Pipeline.py
@@ -3,16 +3,19 @@ import logging
 from networktables import NetworkTable
 
 
-NetworkTable.setIPAddress("10.40.1.2")
+NetworkTable.setIPAddress("roborio-4001-frc.local")
 NetworkTable.setClientMode()
 NetworkTable.initialize()
 logging.basicConfig(level=logging.DEBUG)
 
 #wait for connection
-while not NetworkTable.isConnected:
+while not NetworkTable.isConnected():
     pass
 
+logging.info(str(NetworkTable.isConnected()))
+
 table = NetworkTable.getTable("GearZone")
+
 
 
 # X values of the left boundary of zones, assuming the robot is at the correct distance
@@ -33,38 +36,56 @@ def Pipeline(frame):
     #makes a copy of the original image
     orig = copy.deepcopy(frame)
 
-    #applies filters
-    frame.color_hls()
-    mask = frame.threshold([74, 131, 122], [119, 255, 255])
-    mask.blur(3)
+    #applies filters\
 
-    #if only 1 contour has been found, the robot must be very off
-    if len(mask.contours) == 1:
+    frame.color_hls()
+
+    #mask = frame.threshold([74, 131, 113], [180, 255, 255])
+    mask = frame.threshold([76, 60, 60], [180, 255, 255])
+    mask.blur(3)
+    mask.contours_sort('_area', True)
+
+    #logging.info("# contours --> " + str(len(mask.contours)))
+
+    if len(mask.contours) < 2:
+        #if only 1 contour has been found, the robot must be very off
         table.putValue('zone', -1)
         logging.info(-1)
-    elif len(mask.contours) == 2:
-        liftX = (mask.contours[0].center_x + mask.contours[1].center_x)/2
+
+    else:
+        #mask.contours_sort("area")
+        top_contours = mask.contours[0:2]
+        #for i in range(len(top_contours)):
+        #    logging.info(str(top_contours[i].area))
+
+
+        liftX = (top_contours[0].center_x + top_contours[1].center_x)/2
 
         if liftX < zone1: #left impossible
             table.putValue('zone', -1)
             logging.info(liftX)
             logging.info(-1)
+
         elif liftX > impossibleRight:
             table.putValue('zone', -1)
             logging.info(liftX)
             logging.info("hi")
+
         elif zone1 <= liftX < zone2:
             table.putValue('zone', 1)
             logging.info(liftX)
             logging.info(1)
+
         elif zone2 <= liftX < zone3:
             table.putValue('zone', 2)
             logging.info(liftX)
             logging.info(2)
+
         elif zone3 <= liftX < zone4:
             table.putValue('zone', 3)
             logging.info(liftX)
             logging.info(3)
+
         else:
             table.putValue('zone', 4)
             logging.info(liftX)
